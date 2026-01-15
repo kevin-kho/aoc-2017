5339e38c50b582355916bc15064e567e258e8e4a
Kevin Ho <kevin.ho01@sap.com>
2026-01-15 14:02:40 -0800

    added ConstructNaryTree, CreateWeightMap, and CalculateTreeWeight

--- day07/main.go
+++ day07/main.go
@@ -4,10 +4,17 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/kevin-kho/aoc-utilities/common"
 )
+
+type Disc struct {
+	Name     string
+	Weight   int
+	Children []Disc
+}
 
 func CreateAdjList(data []byte) map[string][]string {
 	adj := make(map[string][]string)
@@ -23,6 +30,26 @@ func CreateAdjList(data []byte) map[string][]string {
 	}
 
 	return adj
+}
+
+func CreateWeightMap(data []byte) (map[string]int, error) {
+	wt := make(map[string]int)
+
+	for entry := range bytes.SplitSeq(data, []byte{'\n'}) {
+		entryStr := string(entry)
+		key := strings.Split(entryStr, " ")[0]
+		i := strings.Index(entryStr, "(")
+		j := strings.Index(entryStr, ")")
+
+		w, err := strconv.Atoi(entryStr[i+1 : j])
+		if err != nil {
+			return wt, err
+		}
+		wt[key] = w
+
+	}
+
+	return wt, nil
 }
 
 func FindRoot(adj map[string][]string) string {
@@ -43,6 +70,45 @@ func FindRoot(adj map[string][]string) string {
 
 }
 
+func ConstructNaryTree(rootName string, adj map[string][]string, wt map[string]int) Disc {
+
+	var construct func(discName string) Disc
+	construct = func(discName string) Disc {
+
+		d := Disc{
+			Name:     discName,
+			Weight:   wt[discName],
+			Children: []Disc{},
+		}
+		for _, c := range adj[discName] {
+			d.Children = append(d.Children, construct(c))
+		}
+		return d
+
+	}
+
+	return construct(rootName)
+
+}
+
+func CalculateTreeWeight(disc Disc) int {
+	var res int
+	queue := []Disc{disc}
+	for len(queue) > 0 {
+		for range len(queue) {
+			d := queue[0]
+			queue = queue[1:]
+
+			res += d.Weight
+			for _, c := range d.Children {
+				queue = append(queue, c)
+			}
+		}
+	}
+
+	return res
+}
+
 func main() {
 	filePath := "./inputExample.txt"
 	filePath = "./input.txt"
@@ -53,7 +119,15 @@ func main() {
 	data = common.TrimNewLineSuffix(data)
 
 	adj := CreateAdjList(data)
+	wt, err := CreateWeightMap(data)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	res := FindRoot(adj)
-	fmt.Println(res)
+	rootName := FindRoot(adj)
+	fmt.Println(rootName)
+
+	tree := ConstructNaryTree(rootName, adj, wt)
+	fmt.Println(tree)
+
 }
