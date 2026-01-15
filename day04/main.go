e20fa62c57e70d7f9f07c617313d293c273505ba
Kevin Ho <kevin.ho01@sap.com>
2026-01-15 11:59:51 -0800

    day04 part1 complete

---
+++ day04/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/kevin-kho/aoc-utilities/common"
+)
+
+func CreatePassPhrases(data []byte) [][]string {
+	var res [][]string
+	for entry := range bytes.SplitSeq(data, []byte{'\n'}) {
+		res = append(res, strings.Split(string(entry), " "))
+	}
+
+	return res
+}
+
+func IsValidPartOne(passPhrase []string) bool {
+	seen := make(map[string]bool)
+	for _, p := range passPhrase {
+		if seen[p] {
+			return false
+		}
+		seen[p] = true
+	}
+
+	return true
+}
+
+func SolvePartOne(passPhrases [][]string) int {
+	var count int
+	for _, p := range passPhrases {
+		if IsValidPartOne(p) {
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	filePath := "./inputExample.txt"
+	filePath = "./input.txt"
+	data, err := common.ReadInput(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data = common.TrimNewLineSuffix(data)
+	passPhrases := CreatePassPhrases(data)
+
+	res := SolvePartOne(passPhrases)
+	fmt.Println(res)
+
+}
